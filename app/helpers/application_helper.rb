a659d03f67c13b63a1af7c02df5bb466e3e81d8a
Ferm0494 <ferm0494@gmail.com>
2020-07-03 14:02:56 -0600

    Added basic helpers for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,22 @@ module ApplicationHelper
 
     false
   end
+
+  def num_languages(user)
+    user.languages.where_not_others_ordered.count
+  end
+
+  def lang_sum_hours_frameworks(lang)
+    lang.frameworks.sum(:total_hours)
+  end
+
+  def lang_frameworks_count(lang)
+    lang.frameworks.count
+  end
+
+  def lang?(language)
+    return true if language.nil?
+
+    false
+  end
 end
